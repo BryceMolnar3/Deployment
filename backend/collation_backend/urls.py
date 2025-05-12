4a3a289635c7e8635860c8cbd9dd94d37ee98291
BryceMolnar <bmolnar@hamilton.edu>
2025-05-11 22:40:44 -0400

    Jesus27

--- backend/collation_backend/urls.py
+++ backend/collation_backend/urls.py
@@ -19,19 +19,21 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('collation.urls')),  # Include collation URLs at /api/
 ]
 
-# Serve React App
+# Serve static files in production
 if not settings.DEBUG:
     urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
         re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
     ]
-
 # Serve static files in development
-if settings.DEBUG:
+else:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
