0b33e392d872047cdec8206f4a92221a20c1ad52
BryceMolnar <bmolnar@hamilton.edu>
2025-05-11 23:41:51 -0400

    Jesus3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { DisplaySettingsProvider } from './contexts/DisplaySettingsContext';
 import ManuscriptViewer from './pages/ManuscriptViewer';
 import VerseId from './pages/verse/VerseId';
@@ -22,22 +22,20 @@ const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) =
 
 function App() {
   return (
-    <Router>
-      <DisplaySettingsProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<Navigate to="/search-database" />} />
-          <Route path="/manuscript-viewer/:sigla" element={<ProtectedRoute><ManuscriptViewer /></ProtectedRoute>} />
-          <Route path="/verse/:verseNumber" element={<ProtectedRoute><VerseId /></ProtectedRoute>} />
-          <Route path="/new-data-entry" element={<ProtectedRoute><NewDataEntry /></ProtectedRoute>} />
-          <Route path="/search-database" element={<ProtectedRoute><SearchDatabase /></ProtectedRoute>} />
-          <Route path="/manual-differentiation" element={<ProtectedRoute><ManualDifferentiation /></ProtectedRoute>} />
-          <Route path="/phylogenetic-analysis" element={<ProtectedRoute><PhylogeneticAnalysis /></ProtectedRoute>} />
-          <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
-          <Route path="*" element={<Navigate to="/search-database" />} />
-        </Routes>
-      </DisplaySettingsProvider>
-    </Router>
+    <DisplaySettingsProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/search-database" />} />
+        <Route path="/manuscript-viewer/:sigla" element={<ProtectedRoute><ManuscriptViewer /></ProtectedRoute>} />
+        <Route path="/verse/:verseNumber" element={<ProtectedRoute><VerseId /></ProtectedRoute>} />
+        <Route path="/new-data-entry" element={<ProtectedRoute><NewDataEntry /></ProtectedRoute>} />
+        <Route path="/search-database" element={<ProtectedRoute><SearchDatabase /></ProtectedRoute>} />
+        <Route path="/manual-differentiation" element={<ProtectedRoute><ManualDifferentiation /></ProtectedRoute>} />
+        <Route path="/phylogenetic-analysis" element={<ProtectedRoute><PhylogeneticAnalysis /></ProtectedRoute>} />
+        <Route path="/settings" element={<ProtectedRoute><Settings /></ProtectedRoute>} />
+        <Route path="*" element={<Navigate to="/search-database" />} />
+      </Routes>
+    </DisplaySettingsProvider>
   );
 }
 
