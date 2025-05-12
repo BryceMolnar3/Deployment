d8b4eff19b0ba5983fa246bd99bbbd1888750d7c
BryceMolnar <bmolnar@hamilton.edu>
2025-05-11 22:17:22 -0400

    Jesus26

--- backend/collation_backend/urls.py
+++ backend/collation_backend/urls.py
@@ -15,14 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('collation.urls')),  # Include collation URLs at root
-    path('api/', include('collation.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),  # Serve React build
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('api/', include('collation.urls')),  # Include collation URLs at /api/
+]
+
+# Serve React App
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    ]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
